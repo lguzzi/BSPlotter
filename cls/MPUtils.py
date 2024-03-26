7cd179498c4a9a7582cc8cc72d0b50efb15cba99
Luca Guzzi <luca.guzzi@mib.infn.it>
2024-03-26 17:46:49 +0100

    temp commit

--- cls/MPUtils.py
+++ cls/MPUtils.py
@@ -5,15 +5,14 @@ import  time, os, sys
 class MPManager:
   ''' manage multiprocess operations
   '''
-  THREADS = 1
-  def __init__(self):
+  def __init__(self, threads=1):
     self.progress = mp.Value('f', 0.0)
 
-  def run_parallel(self, function, iterables):
+  def run_parallel(self, function, iterables, threads):
     ''' run a function in parallel and print a progressbar
     '''
-    with mp.Pool(MPManager.THREADS) as pool:
-      print('[INFO] running parallel on {} jobs'.format(self.threads))
+    with mp.Pool(threads) as pool:
+      print('[INFO] running parallel on {} jobs'.format(threads))
       progressbar = mp.Process(target=self._progressbar)
       progressbar.start()
       results = pool.map(function, iterables)
@@ -27,9 +26,9 @@ class MPManager:
     ''' base function to print a progressbar
     '''
     bar = lambda dim=50: '[{}{}%{}]'.format(
-      '#'*int(self.progress.value*dim/100+1) , 
+      '#'*int(self.progress.value*dim//100) , 
       int(self.progress.value)               , 
-      '-'*int(dim-self.progress.value*dim/100+int(self.progress.value<100)+int(self.progress.value<10))
+      '-'*int(dim-self.progress.value*dim//100+int(self.progress.value<100)+int(self.progress.value<10))
     )
     while(True):
       dim=min(50, os.get_terminal_size()[0]-10)
