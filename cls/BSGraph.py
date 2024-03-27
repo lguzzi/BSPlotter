9c2f94a801dd8d3af0d697a0df4593fc1b05cd71
Luca Guzzi <luca.guzzi@cern.ch>
2024-03-27 16:26:51 +0100

    use native time axes

--- cls/BSGraph.py
+++ cls/BSGraph.py
@@ -1,6 +1,5 @@
 import ROOT
-from datetime import datetime
-class BSGraph (ROOT.TGraph):
+class BSGraph(ROOT.TGraph):
   ''' class for handling the TGraph style
   '''
   def __init__(self, *args, **kwargs):
@@ -11,16 +10,13 @@ class BSGraph (ROOT.TGraph):
     self.SetMarkerColor(ROOT.kBlack)
     self.GetYaxis().SetTitleOffset(1.5)
 
-class BSGraphByTime (BSGraph):
+class BSGraphByTime(BSGraph):
   TIMEFORMAT  = '%Y-%m-%d %H:%M'
-  TIMESTEP    = 60*60*6
   def __init__(self, *args, **kwargs):
     ''' class for handling TGraph with datetime
     x-axis
     '''
     super().__init__(*args, **kwargs)
-
-    for n in range(1, self.GetN()+1):
-      self.GetXaxis().SetBinLabel(
-        n, datetime.fromtimestamp(self.GetXaxis().GetBinCenter(n)).strftime(BSGraphByTime.TIMEFORMAT)
-      )
+    self.GetXaxis().SetTimeDisplay(True)
+    self.GetXaxis().SetTimeOffset(0, 'utc')
+    self.GetXaxis().SetTimeFormat(BSGraphByTime.TIMEFORMAT)
