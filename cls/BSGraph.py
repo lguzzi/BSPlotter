e7e162e519fd0bccf7f5eac349534b71bfc83f76
Luca Guzzi <luca.guzzi@cern.ch>
2024-05-28 15:34:43 +0200

    fix time region

--- cls/BSGraph.py
+++ cls/BSGraph.py
@@ -6,9 +6,9 @@ class BSGraph(ROOT.TGraph):
     super().__init__(*args, **kwargs)
     self.SetMarkerStyle(8)
     self.SetMarkerSize(0.3)
-    self.SetLineColor(ROOT.kBlack)
-    self.SetMarkerColor(ROOT.kBlack)
-    self.GetYaxis().SetTitleOffset(1.5)
+    self.SetLineColor(ROOT.kRed)
+    self.SetMarkerColor(ROOT.kRed)
+    self.GetYaxis().SetTitleOffset(0.7)
 
 class BSGraphByTime(BSGraph):
   TIMEFORMAT  = '%Y-%m-%d %H:%M'
@@ -18,5 +18,5 @@ class BSGraphByTime(BSGraph):
     '''
     super().__init__(*args, **kwargs)
     self.GetXaxis().SetTimeDisplay(True)
-    self.GetXaxis().SetTimeOffset(0, 'utc')
+    self.GetXaxis().SetTimeOffset(0, 'gmt')
     self.GetXaxis().SetTimeFormat(BSGraphByTime.TIMEFORMAT)
